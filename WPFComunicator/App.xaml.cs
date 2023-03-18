029e19fe762add47a2772eb007cfce84dbc586eb
Pele <banskimateusz7@gmail.com>
2023-03-18 13:41:58 +0100

    Initial commit

---
+++ WPFComunicator/App.xaml.cs
@@ -0,0 +1,7 @@
+﻿namespace WPFComunicator {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App {
+    }
+}
---
+++ WPFComunicator/MainWindow.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace WPFComunicator {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
